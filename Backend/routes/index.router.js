e5c3c26a581367ed4fc6dc11b83b146c747b924a
Anandhu_P_A <anandhupa131@gmail.com>
2023-07-20 21:36:07 +0530

    google auth redirection to home page with deployed url implimented

--- Backend/routes/index.router.js
+++ Backend/routes/index.router.js
@@ -6,7 +6,7 @@ const { User } =require("../models/index");
 
 IndexRouter.get("/",async(req,res)=>{
     try {
-         //await User.create({name:"Anandhu",email:"anandhupa131@gmail.com",password:"123456"})
+         await User.create({name:"Anandhu",email:"anandhupa131@gmail.com",password:"123456"})
          let data = await User.findAll();
         res.send(data)
     } catch (error) {
