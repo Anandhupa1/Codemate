df2154f1e2516fdfe2e0c274bc059fe1497349d6
Debraj Roy <debrajroy154@gmail.com>
2023-07-23 22:49:27 +0530

    done

--- Frontend/Client/assets/js/dashboard.js
+++ Frontend/Client/assets/js/dashboard.js
@@ -136,3 +136,17 @@ function updateTableHeaderOnRole(){
   tableHeader.append(tr)
     }
 }
+
+
+
+
+const boxes = document.querySelectorAll(".box");
+const cont = document.getElementById("cont");
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        boxes.forEach((box) => { box.style.backgroundColor = "#5184eb"; box.style.color = "white" })
+
+        box.style.backgroundColor = "white"
+        box.style.color = "black";
+    })
+});
